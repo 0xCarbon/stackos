d3ce5119046eb1c2cbc0944de99a35d9df036176
cadu <cadu@0xcarbon.org>
2022-05-04 11:30:06 -0300

    feat: added governance section for home page

--- src/components/home/section-governance.tsx
+++ src/components/home/section-governance.tsx
@@ -5,9 +5,9 @@ const SectionGovernance = () => (
   <div className="flex flex-col justify-start mx-5">
     <div className="flex flex-row justify-start items-center mb-9">
       <Separator className="h-10 w-[5px] lg:w-2 xl:w-2 2xl:w-[10px] bg-main-green" />
-      <span className="text-[#D9D9D9] font-extrabold text-4xl lg:text-6xl xl:text-6xl 2xl:text-6xl mx-5">
+      <h2 className="text-[#D9D9D9] font-extrabold text-4xl lg:text-6xl xl:text-6xl 2xl:text-6xl mx-5">
         Governance
-      </span>
+      </h2>
     </div>
     <span className="text-white font-normal text-base lg:text-xl xl:text-xl 2xl:text-xl max-w-xl lg:max-w-2xl xl:max-w-2xl 2xl:max-w-3xl mb-3">
       StackOS is a cross-chain open protocol, connecting developers with decentralized compute power
