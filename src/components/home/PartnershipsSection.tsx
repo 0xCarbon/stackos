3051d3444f36092ad948c6507d5e68a76f59824b
vitoalmeida <vitoralmeida.work@gmail.com>
2022-05-10 13:02:47 -0300

    Fix PR threads

--- src/components/home/PartnershipsSection.tsx
+++ src/components/home/PartnershipsSection.tsx
@@ -4,19 +4,9 @@ import { Separator } from '@radix-ui/react-separator';
 import { useTranslation } from 'react-i18next';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { Carousel } from 'react-responsive-carousel';
+import partners from '@/components/home/helpers';
 import LearnMore from './LearnMore';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-const partners = [
-  { id: 1, image: '/assets/home/partner-pinknode.svg', link: 'test' },
-  { id: 2, image: '/assets/home/partner-identomat.svg', link: 'https://identomat.com/' },
-  { id: 3, image: '/assets/home/partner-tapit.svg', link: 'https://www.tapit.kr/' },
-  { id: 4, image: '/assets/home/partner-murall.svg', link: 'https://murall.art/home' },
-  { id: 5, image: '/assets/home/partner-scrow.svg', link: 'https://www.escrowprotocol.app/' },
-  { id: 6, image: '/assets/home/partner-wsninja.svg', link: 'https://www.ws.ninja/' },
-  { id: 7, image: '/assets/home/partner-follow-the-seed.svg', link: 'https://followtheseed.vc/' },
-  { id: 8, image: '/assets/home/partner-saito.svg', link: 'https://saito.io/' },
-];
 
 const PartnershipsSection = () => {
   const { t } = useTranslation();
