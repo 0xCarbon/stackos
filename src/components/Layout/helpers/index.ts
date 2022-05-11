ef6b57c2f0953ea41bfc626c49dce3c06ae295d4
cadu0xcarbon <103645105+cadu0xcarbon@users.noreply.github.com>
2022-05-11 19:41:13 -0300

    feat: added tailwindui navbar (#72)
    * feat: added tailwindui navbar
    
    * fix: navbar margin
    
    * fix: missing tag header

---
+++ src/components/Layout/helpers/index.ts
@@ -0,0 +1,9 @@
+const menus = [
+  { id: 0, name: 'HEADER_TECHNOLOGY', href: '/technology' },
+  { id: 1, name: 'HEADER_DEVELOPERS', href: '/developers' },
+  { id: 2, name: 'HEADER_GOVERNANCE', href: '/governance' },
+  { id: 3, name: 'HEADER_PARTNERS', href: '/partners' },
+  { id: 4, name: 'HEADER_TOKEN', href: '/token' },
+];
+
+export default menus;
