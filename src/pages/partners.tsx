002ffef7f08548ad9dd6f0f45a94ff9e1a709036
Gabriel Abreu <gabriel010101@gmail.com>
2022-05-11 15:15:01 -0300

    Partners main section sem o background

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -1,5 +1,31 @@
+import { Layout } from '@/components';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import type { NextPage } from 'next';
+import Head from 'next/head';
+import PartnersMainSection from '@/components/partners/PartnersMainSection';
 
-const Partners: NextPage = () => <div>TODO</div>;
+const Partners: NextPage = () => (
+  <>
+    <Head>
+      <title>StackOS</title>
+      <meta name="description" content="StackOS partners page" />
+      <meta name="theme-color" content="#111827" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/stackos-logo.svg" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Layout>
+      <PartnersMainSection />
+    </Layout>
+  </>
+);
+
+export async function getStaticProps({ locale }: { locale: any }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
+}
 
 export default Partners;
