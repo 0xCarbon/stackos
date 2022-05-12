3c4f715f80e29651e492561731cbfcd9b4c54cc2
cadu <cadu@0xcarbon.org>
2022-05-12 12:05:52 -0300

    feat: agility section for tech page

---
+++ src/components/technology/helpers/index.ts
@@ -0,0 +1,9 @@
+const features = [
+  { id: 0, title: 'TECHNOLOGY_AGILITY_FEATURE1', subtitle: 'TECHNOLOGY_AGILITY_FEATURE1_TEXT' },
+  { id: 1, title: 'TECHNOLOGY_AGILITY_FEATURE2', subtitle: 'TECHNOLOGY_AGILITY_FEATURE2_TEXT' },
+  { id: 2, title: 'TECHNOLOGY_AGILITY_FEATURE3', subtitle: 'TECHNOLOGY_AGILITY_FEATURE3_TEXT' },
+  { id: 3, title: 'TECHNOLOGY_AGILITY_FEATURE4', subtitle: 'TECHNOLOGY_AGILITY_FEATURE4_TEXT' },
+  { id: 4, title: 'TECHNOLOGY_AGILITY_FEATURE5', subtitle: 'TECHNOLOGY_AGILITY_FEATURE5_TEXT' },
+];
+
+export default features;
