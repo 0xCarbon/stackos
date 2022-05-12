949b80f8b9c844ec0d846f33f5280866e78e4bb6
vitoalmeida <vitoralmeida.work@gmail.com>
2022-05-11 22:01:49 -0300

    Fix spelling

--- src/components/home/PartnershipsSection.tsx
+++ src/components/home/PartnershipsSection.tsx
@@ -46,7 +46,7 @@ const PartnershipsSection = () => {
         <h2 className="text-[#D9D9D9] font-extrabold text-4xl lg:text-6xl mx-5">
           {t('HOME_PARTNERSHIPS_TITLE')}
         </h2>
-        <Separator className="asddasd h-10 lg:h-14 w-[0.4rem] lg:w-2 xl:w-2 2xl:w-[0.45rem] bg-main-green" />
+        <Separator className="h-10 lg:h-14 w-[0.4rem] lg:w-2 xl:w-2 2xl:w-[0.45rem] bg-main-green" />
       </div>
       <p className="text-white font-normal text-base text-right mb-3 lg:mb-6 2xl:mb-10 lg:text-xl max-w-xl lg:max-w-2xl 2xl:max-w-3xl duration-500 ">
         {t('HOME_PARTNERSHIPS_DESCRIPTION')}
