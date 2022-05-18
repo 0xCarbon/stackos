15e3aa0759460d373296d01caca264da43436a67
cadu <cadu@0xcarbon.org>
2022-05-18 16:10:38 -0300

    feat: wagmi setup

--- src/components/token/HeroSection.tsx
+++ src/components/token/HeroSection.tsx
@@ -1,8 +1,19 @@
 import { useTranslation } from 'react-i18next';
+import { useAccount, useConnect, useDisconnect, useEnsAvatar, useEnsName, useNetwork } from 'wagmi';
+
 // import Image from 'next/image';
 import { StackOSButton } from '@/components';
 
 const HeroSection = () => {
+  const { data: account } = useAccount();
+  const { data: ensAvatar } = useEnsAvatar({ addressOrName: account?.address });
+  const { data: ensName } = useEnsName({ address: account?.address });
+  const { connect, connectors, isConnecting, pendingConnector } = useConnect();
+  const { disconnect } = useDisconnect();
+  const { activeChain, chains, isLoading, pendingChainId, switchNetwork } = useNetwork();
+  console.log(chains);
+  console.log(activeChain);
+
   const { t } = useTranslation();
 
   return (
@@ -18,11 +29,54 @@ const HeroSection = () => {
           <p className="text-white font-normal text-xl max-w-lg">{t('TOKEN_HERO_DESCRIPTION')}</p>
         </div>
         <div className="lg:hidden flex flex-row justify-center items-center duration-500">
-          <StackOSButton>BSC</StackOSButton>
+          {/* <StackOSButton>BSC</StackOSButton> */}
+
+          <StackOSButton>
+            {account ? (
+              <div>
+                <img src={ensAvatar} alt="ENS Avatar" />
+                <div>{ensName ? `${ensName} (${account.address})` : account.address}</div>
+                <div>{account?.connector?.name}</div>
+                <button type="button" onClick={() => disconnect()}>
+                  Disconnect
+                </button>
+              </div>
+            ) : (
+              <div>
+                {connectors.map((connector) => (
+                  <button
+                    type="button"
+                    disabled={!connector.ready}
+                    key={connector.id}
+                    onClick={() => connect(connector)}
+                  >
+                    {connector.name}
+                    {!connector.ready && ' (unsupported)'}
+                    {isConnecting && connector.id === pendingConnector?.id && ' (connecting)'}
+                  </button>
+                ))}
+              </div>
+            )}
+          </StackOSButton>
+          <div className="bg-main-green">
+            {activeChain && <div>{activeChain.name}</div>}
+
+            {chains.map((x) => (
+              <button
+                type="button"
+                disabled={!switchNetwork || x.id === activeChain?.id}
+                key={x.id}
+                onClick={() => switchNetwork?.(x.id)}
+              >
+                {x.name}
+                {isLoading && pendingChainId === x.id && ' (switching)'}
+              </button>
+            ))}
+          </div>
         </div>
-        <div className="hidden lg:flex lg:flex-col lg:items-end duration-500">
+        {/* <div className="hidden lg:flex lg:flex-col lg:items-end duration-500">
           <StackOSButton>BSC</StackOSButton>
-        </div>
+        </div> */}
       </div>
       {/* <div className="absolute w-[30.3rem] h-[20.3rem] lg:w-[48.3rem] lg:h-[38.3rem] 2xl:w-[58.3rem] 2xl:h-[48.3rem] right-[-12rem] md:right-[-3rem] lg:right-[-30rem] xl:right-[-10rem] 2xl:right-[-1rem] lg:top-[-3.5rem] 2xl:top-[-4.5rem] duration-500">
         <Image
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,47 @@
 import '../styles/globals.css';
 import React from 'react';
 import type { AppProps } from 'next/app';
-import { Provider } from 'wagmi';
 import { appWithTranslation } from 'next-i18next';
+
+import { Provider, createClient, allChains } from 'wagmi';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+
+const filteredChains = allChains.filter((chain) => chain.id === 1 || chain.id === 137);
+const bscChain = {
+  id: 56,
+  name: 'Binance Smart Chain',
+  nativeCurrency: {
+    name: 'BNB',
+    symbol: 'BNB',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: 'https://bsc-dataseed.binance.org',
+    default2: 'https://bsc-dataseed1.defibit.io/',
+    default3: 'https://bsc-dataseed1.ninicoin.io/',
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'BNB Smart Chain Explorer',
+      url: 'https://bscscan.com',
+    },
+    default: {
+      name: 'BNB Smart Chain Explorer',
+      url: 'https://bscscan.com',
+    },
+  },
+};
+
+const client = createClient({
+  autoConnect: true,
+  connectors() {
+    return [new MetaMaskConnector({ chains: [...filteredChains, bscChain] })];
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider>
+    <Provider client={client}>
       <Component {...pageProps} />
     </Provider>
   );
