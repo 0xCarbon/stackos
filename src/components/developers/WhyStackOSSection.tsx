3508bde25812b9d53de9aae8a2b0552675f97ab3
Gabriel Abreu <gabriel010101@gmail.com>
2022-05-20 00:02:12 -0300

    Performance and adding icons

--- src/components/developers/WhyStackOSSection.tsx
+++ src/components/developers/WhyStackOSSection.tsx
@@ -1,9 +1,36 @@
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
-import ExplainCard from '../ExplainCard';
+import ExplainCard, { Icons } from '../ExplainCard';
 
 const WhyStackOSSection = () => {
   const { t } = useTranslation();
+
+  const explainCards = [
+    {
+      description: 'DEVELOPERS_WHY_EXPLAIN1',
+      iconName: 'rocket',
+    },
+    {
+      description: 'DEVELOPERS_WHY_EXPLAIN2',
+      iconName: 'monitor',
+    },
+    {
+      description: 'DEVELOPERS_WHY_EXPLAIN3',
+      iconName: 'graphs',
+    },
+    {
+      description: 'DEVELOPERS_WHY_EXPLAIN4',
+      iconName: 'docker',
+    },
+    {
+      description: 'DEVELOPERS_WHY_EXPLAIN5',
+      iconName: 'money',
+    },
+    {
+      description: 'DEVELOPERS_WHY_EXPLAIN6',
+      iconName: 'anonymous',
+    },
+  ];
 
   return (
     <div className="relative mt-28">
@@ -13,24 +40,14 @@ const WhyStackOSSection = () => {
           {t('DEVELOPERS_WHY_TITLE')}
         </h2>
         <div className="grid grid-cols-2 lg:grid-cols-3 mt-10 child:my-4 lg:child:my-12 child:max-w-[15rem] md:child:max-w-[17rem] lg:child:max-w-[19rem] xl:child:max-w-[22rem] lg:child:mx-3 xl:child:mx-6 child:text-xl child:font-normal">
-          <div>
-            <ExplainCard description={t('DEVELOPERS_WHY_EXPLAIN1')} />
-          </div>
-          <div>
-            <ExplainCard description={t('DEVELOPERS_WHY_EXPLAIN2')} />
-          </div>
-          <div>
-            <ExplainCard description={t('DEVELOPERS_WHY_EXPLAIN3')} />
-          </div>
-          <div>
-            <ExplainCard description={t('DEVELOPERS_WHY_EXPLAIN4')} />
-          </div>
-          <div>
-            <ExplainCard description={t('DEVELOPERS_WHY_EXPLAIN5')} />
-          </div>
-          <div>
-            <ExplainCard description={t('DEVELOPERS_WHY_EXPLAIN6')} />
-          </div>
+          {explainCards.map((item) => (
+            <div key={item.iconName}>
+              <ExplainCard
+                description={t(item.description)}
+                iconName={item.iconName as keyof Icons}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <div className="absolute w-[47.5rem] h-40 -top-9 -right-40 lg:-right-80 xl:-right-56 2xl:-right-36">
