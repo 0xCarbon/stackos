5e7e943dad32a0de1ca67f4466953d864781a7e4
Gabriel Abreu <84470114+gabeabreu@users.noreply.github.com>
2022-05-25 19:56:03 -0300

    Adding carousel to developers page (#96)
    * Adding carousel to developers page
    
    * Updating lock.json
    
    * Fixing call of carousel
    
    * Carousel bug fixing

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Layout = ({ children }: Props) => (
   <div className="overflow-hidden h-full w-full m-0 flex flex-col bg-main-blue z-0">
-    <div className="w-full max-w-screen-2xl mx-auto">
+    <div className="w-full max-w-screen-2x mx-auto">
       <Header />
       <div className="px-5 md:px-7 lg:px-10 2xl:px-20">{children}</div>
       <Footer />
