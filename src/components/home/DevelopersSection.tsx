687ae665da6b1091c7812239f0906d91ec56fa5d
Gabriel Abreu <gabriel010101@gmail.com>
2022-05-24 22:22:04 -0300

    Developers section background images

--- src/components/home/DevelopersSection.tsx
+++ src/components/home/DevelopersSection.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 import { Card, LearnMore } from '@/components';
 
@@ -5,24 +6,75 @@ const DevelopersSection = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="w-full flex flex-col justify-center items-center text-center py-20 mt-52 md:mt-[33rem] lg:mt-[40rem] xl:mt-[57rem]">
-      <h2 className="text-[#F4F4F4] font-extrabold text-4xl lg:text-6xl mb-9">
-        {t('HOME_DEVELOPERS_TITLE')}
-      </h2>
-      <span className="text-white font-normal text-base max-w-md lg:max-w-lg 2xl:max-w-xl mb-3">
-        {t('HOME_DEVELOPERS_DESCRIPTION')}
-      </span>
-      <LearnMore />
-      <div className="mt-16 flex flex-col justify-center items-center md:flex-row child:mb-10 child:md:mx-4 child:lg:mx-28 child:xl:mx-28 child:2xl:mx-28">
-        <Card
-          src="/assets/home/lorem.svg"
-          title={t('HOME_DEVELOPERS_CARD_TITLE')}
-          subtitle={t('HOME_DEVELOPERS_CARD_SUBTITLE1')}
+    <div className="relative w-full flex flex-col justify-center items-center text-center py-20 mt-[13rem] sm:mt-[16rem] md:mt-[30rem] lg:mt-[40rem] xl:mt-[57rem]">
+      <div className="z-10 flex flex-col justify-center items-center">
+        <h2 className="text-[#F4F4F4] font-extrabold text-4xl lg:text-6xl mb-9">
+          {t('HOME_DEVELOPERS_TITLE')}
+        </h2>
+        <span className="text-white font-normal text-base max-w-md lg:max-w-lg 2xl:max-w-xl mb-3">
+          {t('HOME_DEVELOPERS_DESCRIPTION')}
+        </span>
+        <LearnMore />
+        <div className="mt-16 flex flex-col justify-center items-center md:flex-row child:mb-10 child:md:mx-4 child:lg:mx-28 child:xl:mx-28 child:2xl:mx-28">
+          <Card
+            src="/assets/home/lorem.svg"
+            title={t('HOME_DEVELOPERS_CARD_TITLE')}
+            subtitle={t('HOME_DEVELOPERS_CARD_SUBTITLE1')}
+          />
+          <Card
+            src="/assets/home/ipsum.svg"
+            title={t('HOME_DEVELOPERS_CARD_TITLE')}
+            subtitle={t('HOME_DEVELOPERS_CARD_SUBTITLE2')}
+          />
+        </div>
+        <div className="lg:hidden absolute w-[22.75rem] h-[24.75rem] left-[-8rem] top-[5rem] duration-500">
+          <Image
+            src="/assets/home/developers-background1-sm.svg"
+            alt="technology background"
+            layout="fill"
+          />
+        </div>
+      </div>
+      <div className="md:hidden absolute w-[19.87rem] h-[17.43rem] right-[-4rem] top-[37rem] duration-500">
+        <Image
+          src="/assets/home/developers-background2-sm.svg"
+          alt="technology background"
+          layout="fill"
         />
-        <Card
-          src="/assets/home/ipsum.svg"
-          title={t('HOME_DEVELOPERS_CARD_TITLE')}
-          subtitle={t('HOME_DEVELOPERS_CARD_SUBTITLE2')}
+      </div>
+      <div className="lg:hidden absolute w-[17.5rem] h-[18.25rem] right-[-8rem] top-[50rem] md:right-[-2.8rem] md:top-[46.5rem] duration-500">
+        <Image
+          src="/assets/home/developers-background3-sm.svg"
+          alt="technology background"
+          layout="fill"
+        />
+      </div>
+      <div className="md:hidden absolute w-[18.62rem] h-[19rem] right-[-1.4rem] top-[72rem] md:top-[46.5rem] duration-500">
+        <Image
+          src="/assets/home/developers-background4-sm.svg"
+          alt="technology background"
+          layout="fill"
+        />
+      </div>
+      <div className="hidden md:inline lg:hidden absolute w-[29rem] h-[19rem] left-[-1.4rem] top-[37rem] duration-500">
+        <Image
+          src="/assets/home/developers-background5-md.svg"
+          alt="technology background"
+          layout="fill"
+        />
+      </div>
+      <div className="hidden lg:inline absolute w-[30rem] h-[30rem] left-[-22.4rem] xl:left-[-15rem] 2xl:left-[-7rem] top-[-10rem] duration-500">
+        <Image
+          src="/assets/home/developers-background-lg.svg"
+          alt="technology background"
+          layout="fill"
+        />
+      </div>
+      <div className="hidden lg:inline absolute w-[50rem] h-[50rem] right-[-38.4rem] top-[-15rem] duration-500">
+        <Image
+          src="/assets/home/developers-background2-lg.svg"
+          alt="technology background"
+          layout="fill"
         />
       </div>
     </div>
--- src/components/home/TechnologySection.tsx
+++ src/components/home/TechnologySection.tsx
@@ -8,7 +8,7 @@ const TechnologySection = () => {
 
   return (
     <div className="relative flex flex-col justify-end items-end mt-28 lg:mt-52 2xl:mt-56 py-20">
-      <div className="flex flex-row justify-end items-center mb-9 child:duration-500">
+      <div className="flex flex-row justify-end items-center mb-9 child:duration-500 z-10">
         <div className="justify-end flex flex-col space-y-3">
           <h2 className="text-[#D9D9D9] font-light text-sm lg:text-4xl mx-5 text-right">
             {t('HOME_TECHNOLOGY_SUBTITLE')}
@@ -19,7 +19,7 @@ const TechnologySection = () => {
         </div>
         <Separator className="h-20 lg:h-28 w-[0.4rem] lg:w-2 xl:w-2 2xl:w-[0.45rem] bg-main-green" />
       </div>
-      <div className="max-w-[30rem] lg:max-w-2xl">
+      <div className="max-w-[30rem] lg:max-w-2xl z-10">
         <p className="text-white font-normal text-base text-right mb-3 lg:mb-6 2xl:mb-10 lg:text-xl max-w-xl lg:max-w-2xl 2xl:max-w-3xl duration-500 ">
           {t('HOME_TECHNOLOGY_DESCRIPTION')}
         </p>
@@ -30,7 +30,6 @@ const TechnologySection = () => {
           src="/assets/home/technology-background1-sm.svg"
           alt="technology background"
           layout="fill"
-          priority
         />
       </div>
       <div className="hidden md:inline absolute w-[30.3rem] h-[20.3rem] left-[-5.5rem] xl:left-[4rem] top-[-0.5rem] duration-500">
@@ -38,7 +37,6 @@ const TechnologySection = () => {
           src="/assets/home/technology-background1.svg"
           alt="technology background"
           layout="fill"
-          priority
         />
       </div>
       <div className="lg:hidden absolute w-[15rem] h-[21rem] left-[-5rem] top-[17rem]  duration-500">
@@ -46,7 +44,6 @@ const TechnologySection = () => {
           src="/assets/home/technology-background2-sm.svg"
           alt="technology background"
           layout="fill"
-          priority
         />
       </div>
       <div className="hidden md:inline lg:hidden absolute w-[2.37rem] h-[2.37rem] left-[6rem] top-[16rem]  duration-500">
@@ -54,7 +51,6 @@ const TechnologySection = () => {
           src="/assets/home/technology-background2-md.svg"
           alt="technology background"
           layout="fill"
-          priority
         />
       </div>
       <div className="hidden md:inline lg:hidden absolute w-[0.7rem] h-[0.7rem] left-[15rem] top-[16rem]  duration-500">
@@ -62,7 +58,6 @@ const TechnologySection = () => {
           src="/assets/home/technology-background3-md.svg"
           alt="technology background"
           layout="fill"
-          priority
         />
       </div>
       <div className="hidden lg:inline absolute w-[51.5rem] h-[41rem] left-[-10rem] top-[24rem] xl:top-[42rem] xl:left-[0rem] duration-500">
@@ -70,11 +65,10 @@ const TechnologySection = () => {
           src="/assets/home/technology-background4-lg.svg"
           alt="technology background"
           layout="fill"
-          priority
         />
       </div>
       <div className="absolute flex w-[18.75rem] h-[16.87rem] top-[30rem] md:w-[40rem] md:h-[36rem] md:top-[25rem] lg:w-[48.75rem] lg:h-[43.75rem] lg:top-[28rem] xl:w-[65.75rem] xl:h-[59rem] xl:top-[29rem] mx-auto right-0 left-0 duration-500">
-        <Image src="/assets/home/diagram.svg" alt="technology background" layout="fill" />
+        <Image src="/assets/home/diagram.svg" alt="technology background" layout="fill" priority />
       </div>
     </div>
   );
