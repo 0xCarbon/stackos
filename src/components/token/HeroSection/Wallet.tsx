15bb795b5b394febce29f6fe7ffd96cb31563a1d
cadu <cadu@0xcarbon.org>
2022-05-26 21:47:01 -0300

    wagmi setup hell

--- src/components/token/HeroSection/Wallet.tsx
+++ src/components/token/HeroSection/Wallet.tsx
@@ -1,10 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useAccount, useConnect, useDisconnect, useNetwork } from 'wagmi';
+import { useAccount, useConnect, useNetwork, useProvider } from 'wagmi';
 
 // import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { providers, ethers } from 'ethers';
+import { hexStripZeros } from '@ethersproject/bytes';
+import { BigNumber } from '@ethersproject/bignumber';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { SwapWidget } from '@uniswap/widgets';
+
 import { HiSwitchVertical } from 'react-icons/hi';
 import { StackOSButton, StackOSDropdown, StackOSIcon, StackOSInput } from '@/components';
+
+// VER O COMPONENTE DO VITOR
 
 interface Token {
   id: number;
@@ -13,16 +22,12 @@ interface Token {
   subtitle?: string;
 }
 
-interface Tokens {
-  Ethereum: Token[];
-  Polygon: Token[];
-  BSC: Token[];
-}
+const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
 
 const Wallet = () => {
-  const { data: account } = useAccount();
-  const { connect, connectors, isConnecting, pendingConnector } = useConnect();
-  const { disconnect } = useDisconnect();
+  // const { data: account } = useAccount();
+
+  const { connect, connectors } = useConnect();
   const { activeChain, chains, switchNetwork } = useNetwork();
 
   const metamask = connectors[0];
@@ -35,51 +40,49 @@ const Wallet = () => {
   const polygonChain = chains.find((chain) => chain.id === POLYGON);
 
   const networkOptions = [
+    ...chains,
     { title: 'Ethereum', subtitle: 'Uniswap (v2)', icon: 'uniswap', ...ethereumChain },
     { title: 'Polygon', subtitle: 'Dfyn', icon: 'dfyn', ...polygonChain },
     { title: 'BSC', subtitle: 'PancakeSwap (v2)', icon: 'pancakeswap', ...binanceChain },
   ];
 
-  const [tokenOptions, setTokenOptions] = useState<Token[]>([
-    { id: 1, title: 'ETH', icon: 'eth' },
-    { id: 2, title: 'USDC', icon: 'usdc' },
-    { id: 3, title: 'USDT', icon: 'usdt' },
-    { id: 43, title: 'WETH', icon: 'weth' },
-  ]);
+  const jsonRpcEndpoint = `${ethereumChain?.rpcUrls.alchemy}/${apiKey}`;
+  const provider = new ethers.providers.AlchemyProvider('homestead', apiKey);
+  // const provider = useProvider({ chainId: 1 });
+  console.log(provider);
+
+  const [account, setAccount] = useState({
+    address: '',
+    provider,
+  });
+
+  async function connectWallet() {
+    //check if Metamask is installed in the browser
+    const ethereumProvider = await detectEthereumProvider();
+    if (ethereumProvider) {
+      // connect(metamask);
+
+      //prompt user to connect their wallet
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      const address = accounts[0];
+      setAccount({
+        address,
+        provider: ethereumProvider,
+      });
+    }
+  }
+
   const [networkSelected, setNetworkSelected] = useState<Token>({
     id: 56,
     title: 'BSC',
     icon: 'pancakeswap',
   });
-  const [tokenSelected, setTokenSelected] = useState<Token>({ id: 1, title: 'ETH', icon: 'eth' });
 
   useEffect(() => {
     setupNetwork();
   }, []);
-
-  useEffect(() => {
-    const tokens: Tokens = {
-      Ethereum: [
-        { id: 1, title: 'ETH', icon: 'eth' },
-        { id: 2, title: 'USDC', icon: 'usdc' },
-        { id: 3, title: 'USDT', icon: 'usdt' },
-        { id: 43, title: 'WETH', icon: 'weth' },
-      ],
-      Polygon: [
-        { id: 1, title: 'MATIC', icon: 'matic' },
-        { id: 2, title: 'USDC', icon: 'usdc' },
-        { id: 3, title: 'USDT', icon: 'usdt' },
-      ],
-      BSC: [
-        { id: 1, title: 'BNB', icon: 'bnb' },
-        { id: 2, title: 'BUSD', icon: 'busd' },
-        { id: 3, title: 'USDT', icon: 'usdt' },
-        { id: 43, title: 'WBNB', icon: 'wbnb' },
-      ],
-    };
-
-    setTokenOptions(tokens[networkSelected.title as keyof Tokens]);
-  }, [networkSelected]);
 
   useEffect(() => {
     setupNetwork();
@@ -93,58 +96,39 @@ const Wallet = () => {
     switchNetwork?.(value);
   }
 
-  function onChangeToken(value: number) {
-    setTokenSelected(tokenOptions.find((option) => option.id === value) as Token);
-  }
-
   function setupNetwork() {
     if (activeChain && activeChain?.id !== networkSelected?.id) {
       setNetworkSelected(networkOptions.find((option) => option.id === activeChain?.id) as Token);
     }
   }
 
+  console.log(account);
+
   return (
     <>
-      <StackOSDropdown
-        className="w-full h-12 px-2 bg-main-green flex flex-row justify-center items-center rounded"
-        header="Select a network"
-        selected={networkSelected.id}
-        dropdownOptions={networkOptions}
-        onChangeSelection={(value) => onChangeNetwork(value)}
-      >
-        <StackOSIcon className="h-5 w-5" iconName={networkSelected.icon} />
-        <span className="text-[#111827] font-medium text-base ml-2 mr-3">
-          {networkSelected.title}
-        </span>
-      </StackOSDropdown>
-      <div className="mt-20 p-4 bg-[#1F2937] rounded-md">
-        <p className="text-[#F9FAFB] font-semibold text-xl mb-8">Buy STACK</p>
-        <StackOSInput
-          dropdownOptions={tokenOptions}
-          selected={tokenSelected.id}
-          onChangeSelection={(value) => onChangeToken(value)}
-        />
-        <div className="relative z-10 h-1 flex flex-row justify-center items-center">
-          <HiSwitchVertical color="#84CC16" size={20} />
-        </div>
-        <StackOSInput />
-        <div className="flex flex-row justify-center items-center mt-6 w-full">
-          {account?.address ? (
-            <div className="w-full child:w-full" onClick={() => disconnect()}>
-              <StackOSButton>Buy STACK</StackOSButton>
-            </div>
-          ) : (
-            <button
-              type="button"
-              className="w-full bg-transparent border border-main-green text-main-green rounded-md px-9 py-3"
-              onClick={() => connect(metamask)}
-            >
-              {isConnecting && metamask?.id === pendingConnector?.id
-                ? 'Connecting Wallet...'
-                : 'Connect Wallet'}
-            </button>
-          )}
-        </div>
+      <div className="flex flex-row">
+        <button
+          type="button"
+          className="w-full bg-transparent border border-main-green text-main-green rounded-md px-9 py-3"
+          onClick={() => connectWallet()}
+        >
+          {account ? 'Connected' : 'Connect Wallet'}
+        </button>
+        <StackOSDropdown
+          className="w-full h-12 px-2 bg-main-green flex flex-row justify-center items-center rounded"
+          header="Select a network"
+          selected={networkSelected.id}
+          dropdownOptions={networkOptions}
+          onChangeSelection={(value) => onChangeNetwork(value)}
+        >
+          <StackOSIcon className="h-5 w-5" iconName={networkSelected.icon} />
+          <span className="text-[#111827] font-medium text-base ml-2 mr-3">
+            {networkSelected.title}
+          </span>
+        </StackOSDropdown>
+      </div>
+      <div className="Uniswap">
+        <SwapWidget provider={account.provider} jsonRpcEndpoint={jsonRpcEndpoint} />
       </div>
     </>
   );
