e553d6819c64c8be68b5fdb107ef466acad7ca95
Gabriel Abreu <gabriel010101@gmail.com>
2022-06-01 14:32:48 -0300

    Footer responsivity with links bug

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -7,29 +7,31 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col">
-      <div>
-        <h1 className="text-[1.9rem] font-semibold text-main-green">{t('FOOTER_TITLE')}</h1>
-        <div className="flex text-sm font-extralight text-main-grey">
-          <div className="flex mr-4">
-            <Image src="/assets/layout/paper.svg" width={9} height={12} />
-            <p className="ml-[0.44rem]">{t('FOOTER_WHITEPAPER')}</p>
+    <div>
+      <div className="flex flex-col md:flex-row md:justify-between lg:mt-56 px-5 md:px-0 max-w-[23rem] sm:max-w-[33.75rem] md:max-w-[45rem] lg:max-w-[60rem] xl:max-w-[71.25rem] mx-auto">
+        <div>
+          <h1 className="text-[1.9rem] font-semibold text-main-green">{t('FOOTER_TITLE')}</h1>
+          <div className="flex text-sm font-extralight text-main-grey">
+            <div className="flex mr-4">
+              <Image src="/assets/layout/paper.svg" width={9} height={12} />
+              <p className="ml-[0.44rem]">{t('FOOTER_WHITEPAPER')}</p>
+            </div>
+            <div className="flex">
+              <Image src="/assets/layout/ambassador.svg" width={10} height={12} />
+              <p className="ml-[0.44rem]">{t('FOOTER_AMBASSADOR')}</p>
+            </div>
           </div>
-          <div className="flex">
-            <Image src="/assets/layout/ambassador.svg" width={10} height={12} />
-            <p className="ml-[0.44rem]">{t('FOOTER_AMBASSADOR')}</p>
+        </div>
+        <div className="flex text-main-green text-2xl space-x-5 mt-7 child:p-3 child:rounded-full child:border child:border-main-green">
+          <div>
+            <BsTwitter />
           </div>
-        </div>
-      </div>
-      <div className="flex text-main-green text-2xl space-x-5 child:p-3 child:rounded-full child:border child:border-main-green">
-        <div>
-          <BsTwitter />
-        </div>
-        <div>
-          <BsTelegram />
-        </div>
-        <div>
-          <FaDiscord />
+          <div>
+            <BsTelegram />
+          </div>
+          <div>
+            <FaDiscord />
+          </div>
         </div>
       </div>
       <div className="py-6 border-t border-main-green text-center">
